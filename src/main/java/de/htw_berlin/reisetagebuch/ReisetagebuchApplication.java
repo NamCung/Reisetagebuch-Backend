f1c311fc772c69fafe1f932136299afb4baed840
Vu Thang Bui <Vu.Bui@Student.HTW-Berlin.de>
2026-04-14 19:13:43 +0200

    Initial commit

---
+++ src/main/java/de/htw_berlin/reisetagebuch/ReisetagebuchApplication.java
@@ -0,0 +1,13 @@
+package de.htw_berlin.reisetagebuch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReisetagebuchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReisetagebuchApplication.class, args);
+	}
+
+}
---
+++ src/test/java/de/htw_berlin/reisetagebuch/ReisetagebuchApplicationTests.java
@@ -0,0 +1,13 @@
+package de.htw_berlin.reisetagebuch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReisetagebuchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
