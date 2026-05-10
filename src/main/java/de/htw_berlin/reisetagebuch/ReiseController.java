4ce319c916bcbffbf916655c3b27f0ae87485a3c
namcu <Luis.Cung@Student.HTW-Berlin.de>
2026-05-10 13:08:11 +0200

    Cors

--- src/main/java/de/htw_berlin/reisetagebuch/ReiseController.java
+++ src/main/java/de/htw_berlin/reisetagebuch/ReiseController.java
@@ -1,9 +1,11 @@
 package de.htw_berlin.reisetagebuch;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import java.util.List;
 
 @RestController
+@CrossOrigin(origins = "https://reisetagebuch-frontend-1.onrender.com")
 public class ReiseController {
     @GetMapping("/countries")
     public List<VisitedCountry> getAllCountries() {
