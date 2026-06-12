ee76172227f2aff5677043b379015fd61053fd77
Vu Thang Bui <Vu.Bui@Student.HTW-Berlin.de>
2026-06-12 09:32:53 +0200

    M4

--- src/main/java/de/htw_berlin/reisetagebuch/ReiseController.java
+++ src/main/java/de/htw_berlin/reisetagebuch/ReiseController.java
@@ -1,20 +1,25 @@
 package de.htw_berlin.reisetagebuch;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.bind.annotation.CrossOrigin;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
 import java.util.List;
 
 @RestController
-@CrossOrigin(origins = "https://reisetagebuch-frontend-1.onrender.com")
 public class ReiseController {
+
+    @Autowired
+    CountryService service;
+
+    @CrossOrigin
     @GetMapping("/countries")
     public List<VisitedCountry> getAllCountries() {
-        return List.of(
-                new VisitedCountry("Deutschland", "DE"),
-                new VisitedCountry("Frankreich", "FR"),
-                new VisitedCountry("Japan", "JP"),
-                new VisitedCountry("USA", "US"),
-                new VisitedCountry("Australien", "AU")          // Kommentar Test
-        );
+        return service.getAll();
+    }
+
+    @CrossOrigin
+    @PostMapping("/countries")
+    public VisitedCountry addCountry(@RequestBody VisitedCountry country) {
+        return service.save(country);
     }
 }
--- src/main/java/de/htw_berlin/reisetagebuch/VisitedCountry.java
+++ src/main/java/de/htw_berlin/reisetagebuch/VisitedCountry.java
@@ -1,26 +1,36 @@
 package de.htw_berlin.reisetagebuch;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
 public class VisitedCountry {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    Long id;
     String country;
     String countryCode;         // ISO Code für Länder JP- Japan
 
-
-    public VisitedCountry(){}
+    public VisitedCountry() {}
 
     public VisitedCountry(String country, String countryCode) {
-
         this.country = country;
         this.countryCode = countryCode;
-
     }
 
+    public Long getId() { return id; }
+    public void setId(Long id) { this.id = id; }
 
     public String getCountry() { return country; }
     public void setCountry(String country) { this.country = country; }
 
     public String getCountryCode() { return countryCode; }
-    public void setCountryCode(String countryCode) { this.countryCode = countryCode; }}
+    public void setCountryCode(String countryCode) { this.countryCode = countryCode; }
 
-
-
-
+    @Override
+    public String toString() {
+        return "VisitedCountry{id=" + id + ", country='" + country + "', countryCode='" + countryCode + "'}";
+    }
+}
